0060151184218d664a65fc7c92ea8200fbd933b7
solbiko <gsb0904@hanmail.net>
2023-01-10 23:02:22 +0900

    apiConfig 수정

--- src/lib/apiConfig.js
+++ src/lib/apiConfig.js
@@ -1,4 +1,8 @@
 const service = {
+    user : "localhost:8001",
+    result : "localhost:8002",
+    requestMsg : "localhost:8011",
+    requestEmail : "localhost:8012",
 };
 
 const version = '/v1';
