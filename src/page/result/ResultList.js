a25ce57659199baf32ee23e16e825af7e1bd7f67
solbiko <gsb0904@hanmail.net>
2023-01-13 11:14:11 +0900

    발송 결과 페이지 페이징 컴포넌트 추가

--- src/page/result/ResultList.js
+++ src/page/result/ResultList.js
@@ -17,7 +17,9 @@ import {
   CFormSelect,
   CButton,
   CFormInput,
-  CInputGroupText
+  CInputGroupText,
+  CPagination,
+  CPaginationItem,
 } from '@coreui/react'
 
 
@@ -158,6 +160,18 @@ function ResultList() {
                     </CTableBody>
                 </CTable>
               </CCard>
+
+              <CPagination align="center" aria-label="Page navigation example">
+                <CPaginationItem aria-label="Previous" disabled>
+                  <span aria-hidden="true">&laquo;</span>
+                </CPaginationItem>
+                <CPaginationItem active>1</CPaginationItem>
+                <CPaginationItem>2</CPaginationItem>
+                <CPaginationItem>3</CPaginationItem>
+                <CPaginationItem aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                </CPaginationItem>
+              </CPagination>
             </CCardBody>
         </CCard>
 
