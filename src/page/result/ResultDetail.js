d684e0ae9807bafc434f0b912f9bb0e7200815fa
sebin <sbscsue@gmail.com>
2023-01-13 16:36:12 +0900

    [FIX]결과 상세 조회 수정

--- src/page/result/ResultDetail.js
+++ src/page/result/ResultDetail.js
@@ -24,6 +24,24 @@ function Graph() {
     return (  
         <div>
             <CRow>
+                <h1>발송 정보</h1>
+            </CRow>
+            <CRow>
+                <CCol xs={4}>
+                    <p>발송타입 : sms</p>
+                    <p>개수(건) : 1000</p>
+                    <p>등록 시간 : 23-01-06 11:39:11</p>
+                    <p>예약 시간 : -</p>
+                    <p>성공 여부 : 성공</p>
+                    <p>실패 개수 : 0</p>
+                    <p>평균 속도(초): 0.1</p>
+                    <p>완료 시간 : 23-01-06 11:39:30</p>
+                </CCol>
+            </CRow>
+            <CRow>
+                <h1>중계사 정보</h1>
+            </CRow>
+            <CRow>
                 <CCol xs={4}>
                     <CCard className="mb-4">
                         <CCardHeader>분배 비율</CCardHeader>
@@ -91,55 +109,7 @@ function Result(){
     return(
         <div>
             <CRow>
-                <CCol xs={5}>
-                    <CRow>
-                        <h1>발송통계</h1>
-                    </CRow>
-                    <CRow>
-                        <CTable>
-                        <CTableHead>
-                        <CTableRow color="primary">
-                            <CTableHeaderCell scope="col">#</CTableHeaderCell>
-                            <CTableHeaderCell scope="col">결과</CTableHeaderCell>
-                            <CTableHeaderCell scope="col">건수</CTableHeaderCell>
-                        </CTableRow>
-                        </CTableHead>
-                        <CTableBody>
-                        <CTableRow>
-                            <CTableDataCell>1</CTableDataCell>
-                            <CTableDataCell>~한 이유로 성공</CTableDataCell>
-                            <CTableDataCell>1000</CTableDataCell>
-                        </CTableRow>
-                        <CTableRow>
-                            <CTableDataCell>2</CTableDataCell>
-                            <CTableDataCell>~한 이유로 실패</CTableDataCell>
-                            <CTableDataCell>1000</CTableDataCell>
-                        </CTableRow>
-                        <CTableRow>
-                            <CTableDataCell>3</CTableDataCell>
-                            <CTableDataCell>~한 이유로 성공</CTableDataCell>
-                            <CTableDataCell>1000</CTableDataCell>
-                        </CTableRow>
-                        </CTableBody>
-                        </CTable>
-                    </CRow>
-                    <CRow className="mb-3 justify-content-center">
-                        <CCol lg={12}>
-                        <CPagination align="center" aria-label="Page navigation example">
-                            <CPaginationItem aria-label="Previous">
-                            <span aria-hidden="true">&laquo;</span>
-                            </CPaginationItem>
-                            <CPaginationItem>1</CPaginationItem>
-                            <CPaginationItem>2</CPaginationItem>
-                            <CPaginationItem>3</CPaginationItem>
-                            <CPaginationItem aria-label="Next">
-                            <span aria-hidden="true">&raquo;</span>
-                            </CPaginationItem>
-                        </CPagination>
-                        </CCol>
-                    </CRow>
-                </CCol>
-                <CCol xs={7}>
+                <CCol xs={12}>
                     <CRow>
                         <h1>발송 결과</h1>
                     </CRow>
@@ -150,6 +120,8 @@ function Result(){
                                 <CTableHeaderCell scope="col">수신번호</CTableHeaderCell>
                                 <CTableHeaderCell scope="col">전송상태</CTableHeaderCell>
                                 <CTableHeaderCell scope="col">상세사유</CTableHeaderCell>
+                                <CTableHeaderCell scope="col">전송제목</CTableHeaderCell>
+                                <CTableHeaderCell scope="col">전송내용</CTableHeaderCell>
                             </CTableRow>
                             </CTableHead>
                             <CTableBody>
@@ -157,16 +129,22 @@ function Result(){
                                 <CTableDataCell>010-2324-8257</CTableDataCell>
                                 <CTableDataCell>결과 수신 실패</CTableDataCell>
                                 <CTableDataCell>중계사 혼잡</CTableDataCell>
+                                <CTableDataCell>올리브영 광고</CTableDataCell>
+                                <CTableDataCell>김세빈 고객님에게...</CTableDataCell>
                             </CTableRow>
                             <CTableRow>
                                 <CTableDataCell>010-2324-8257</CTableDataCell>
                                 <CTableDataCell>결과 수신 실패</CTableDataCell>
                                 <CTableDataCell>중계사 혼잡</CTableDataCell>
+                                <CTableDataCell>올리브영 광고</CTableDataCell>
+                                <CTableDataCell>김네빈 고객님에게...</CTableDataCell>
                             </CTableRow>
                             <CTableRow>
                                 <CTableDataCell>010-2324-8257</CTableDataCell>
                                 <CTableDataCell>결과 수신 실패</CTableDataCell>
                                 <CTableDataCell>중계사 혼잡</CTableDataCell>
+                                <CTableDataCell>올리브영 광고</CTableDataCell>
+                                <CTableDataCell>김다섯빈 고객님에게...</CTableDataCell>
                             </CTableRow>
                             </CTableBody>
                         </CTable>
