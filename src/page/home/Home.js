36d1abc970cc2d6dd53713237f3bacb9a19943c7
Karla <karlako9482@gmail.com>
2023-02-14 13:47:52 +0900

    메인 수정

--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {CButton, CImage, CRow, CCallout,CCarousel, CCarouselItem, CCarouselCaption, CCard, CCardHeader, CCardBody, CCardTitle, CCardText, CCardFooter, CCardImage} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+
 import HomeHeader from './HomeHeader';
 import "./home.scss";
-import mainImg from "../../assets/images/home/kite1.png";
-import teamInfo from "../../assets/images/home/team.png";
+import mainImg1 from "../../assets/images/home/kite0.png";
+import mainImg2 from "../../assets/images/home/kite1.png";
 
-
+import { cilPaperPlane } from '@coreui/icons';
 
 import BrokerInfoCardGroup from "./component/BrokerInfoCardGroup";
 
@@ -18,24 +20,26 @@ const Home = () => {
   return (
     <>
       <HomeHeader></HomeHeader>
-      <div className="home-wrapper mb-5">
+      <div className="home-wrapper mb-2">
 
         {/* 메인이미지 */}
         <div className="home-title">
-            <CImage className='mt-1 main_img' src={mainImg}/>
+            <CImage className='mt-5 main_img' src={mainImg1}/>
+            <div className="d-grid gap-2 col-6 mx-auto">
+              <CButton className='main_button' color="light" href="/#/sendSms" shape="rounded-pill" variant="outline" >
+                <CIcon icon={cilPaperPlane} height={10}/>
+                <span> 문자 발송하기</span>
+              </CButton>
+            </div>
+        </div>
+        <div className="home-title">
+          <CImage className='main_img' src={mainImg2}/>
         </div>
 
-
-      <div className="home-contents">
-        {/* 발송하기버튼 */}
-        <div className="d-grid gap-2 col-6 mx-auto mt-3 mb-5">
-          <CButton size="lg" color="primary" href="/#/sendSms" shape="rounded-pill" >문자 발송하기</CButton>
-        </div>
-
-        {/* 중계사 */}
-        <span>중계사 정보</span>
-          <BrokerInfoCardGroup/>
-        </div>
+      {/* <div className="home-contents"> */}
+        {/* <span>중계사 정보</span> */}
+        {/* <BrokerInfoCardGroup/> */}
+      {/* </div> */}
 
         <div className="website">
           <span className='my-website-title me-1'> Guts&Gun</span>
